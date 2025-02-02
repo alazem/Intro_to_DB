9896cde9136d484b744690bcbb491484bbc18757
Alazar Zemene <alazarzem2022@gmail.com>
2025-02-02 20:26:28 +0300

    update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -8,14 +8,14 @@ CREATE TABLE IF NOT EXISTS Books(
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title  VARCHAR(130) NOT NULL,
     author_id INT, 
-    Price INT,
+    Price DOUBLE,
     Publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(215) NOT NULL,
-    email VARCHAR(215) UNIQUE NOT NULL,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
     address TEXT
 );
 
@@ -31,7 +31,8 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id INT,
-    quantity DOUBLE NOT NULL,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id) ON DELETE CASCADE,
     FOREIGN KEY (book_id) REFERENCES Books(book_id) ON DELETE CASCADE
 );
+ALTER TABLE Books MODIFY COLUMN price DOUBLE NOT NULL;
